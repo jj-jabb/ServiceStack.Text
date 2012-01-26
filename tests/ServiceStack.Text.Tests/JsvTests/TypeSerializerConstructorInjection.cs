0eef137da77300f69ceb666661592cf7bc12cf02
jj-jabb <jj.jabbaloo@gmail.com>
2012-01-25 22:19:00 -0700

    added test for constructor injection

---
+++ tests/ServiceStack.Text.Tests/JsvTests/TypeSerializerConstructorInjection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ServiceStack.Text.Tests.JsvTests
+{
+    interface IFoo
+    {
+        Guid Id { get; set; }
+        List<IBar> Bars { get; set; }
+    }
+
+    interface IBar
+    {
+        string Name { get; set; }
+        int Amount { get; set; }
+    }
+
+    class Foo : IFoo
+    {
+        public Foo()
+        {
+            Bars = new List<IBar>();
+        }
+
+        public Guid Id { get; set; }
+        public List<IBar> Bars { get; set; }
+    }
+
+    class Bar : IBar
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+
+    [TestFixture]
+    public class TypeSerializerConstructorInjection
+    {
+
+        [Test]
+        public void Can_inject_constructor()
+        {
+            JsConfig.ExcludeTypeInfo = true;
+            TypeConfig<IFoo>.Constructor = () => new Foo();
+            TypeConfig<IBar>.Constructor = () => new Bar();
+
+
+            var expectedFoo = new Foo { Id = new Guid("{51517605-8c5e-470d-8516-4da01a0ddeab}") };
+            expectedFoo.Bars.Add(new Bar { Name = "Apple", Amount = 4 });
+            expectedFoo.Bars.Add(new Bar { Name = "Orange", Amount = 6 });
+
+            var expectedFooStr = TypeSerializer.SerializeToString<Foo>(expectedFoo);
+            var testFoo = TypeSerializer.DeserializeFromString<Foo>(expectedFooStr);
+
+            var actualFoo = TypeSerializer.DeserializeFromString<IFoo>(expectedFooStr);
+
+            var actualFooStr = TypeSerializer.SerializeToString<Foo>((Foo)actualFoo);
+
+            Assert.AreEqual(expectedFooStr, actualFooStr);
+        }
+    }
+}
